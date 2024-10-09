0c645ce72c61d47525b3baf347fd0740f496c854
JioJio <>
2024-10-10 03:32:38 +0800

    部分文字处理
    Signed-off-by: JioJio <>

--- whoTranslation.js
+++ whoTranslation.js
@@ -16,6 +16,8 @@ const whoTranslation = {
         },
         global: {
             title: "At a glance",
+            globalFootprintTitle: "ICG’s global footprint",
+            globalFootprintDescription: "Operating out of 19 locations.",
             stats: [
                 {
                     value: "$101bn",
@@ -32,8 +34,114 @@ const whoTranslation = {
                     description: "Worldwide locations",
                     subtext: ""
                 }
-            ]
-        }
+            ],
+        },
+        mapPoints: [
+            {
+                title: "London",
+                subtitle: "Procession House, 55 Ludgate Hill, London, EC4M 7JW, England, United Kingdom",
+                description: "The UK capital is home to our global headquarters."
+            },
+            {
+                title: "Amsterdam",
+                subtitle: "Paulus Potterstraat 20, 2hg., Amsterdam, 1071 DA, Netherlands",
+                description: ""
+            },
+            {
+                title: "Luxembourg",
+                subtitle: "Private Business Centre, 32-36 Boulevard d’Avranches, L-1160, Luxembourg",
+                description: ""
+            },
+            {
+                title: "Madrid",
+                subtitle: "Serrano, 30-3°, 28001, Madrid, Spain",
+                description: "We opened an office in Madrid in 2003."
+            },
+            {
+                title: "Milan",
+                subtitle: "Corso Giacomo Matteotti 3, CAP 20121 Milano, Italy",
+                description: "We opened an office in Milan in 2020."
+            },
+            {
+                title: "Paris",
+                subtitle: "1, rue de la Paix, 75002, Paris, France",
+                description: ""
+            },
+            {
+                title: "Stockholm",
+                subtitle: "David Bagares Gata, 3, 111 38 Stockholm, Sweden",
+                description: ""
+            },
+            {
+                title: "Warsaw",
+                subtitle: "Spark B (7th floor), Aleja Solidarności 171 St., 00-877 Warsaw, Poland",
+                description: ""
+            },
+            {
+                title: "New York",
+                subtitle: "277 Park Avenue, New York, NY 10172, USA",
+                description: "We opened an office in New York in 2007."
+            },
+            {
+                title: "Toronto",
+                subtitle: "161 Bay Street, Suite 2700, Toronto, Ontario, M5J 2S1",
+                description: "We opened an office in Toronto in 2024."
+            },
+            {
+                title: "San Francisco",
+                subtitle: "",
+                description: ""
+            },
+            {
+                title: "Dubai",
+                subtitle: "Index Tower, Floor 4, Unit 404, Dubai International Financial Centre, Dubai, United Arab Emirates",
+                description: "We opened our office in Dubai in 2021."
+            },
+            {
+                title: "Hong Kong",
+                subtitle: "Suites 1301 – 1302, AIA Central, 1 Connaught Road Central, Hong Kong",
+                description: "We opened our first Asia Pacific office, in Hong Kong, in 2001."
+            },
+            {
+                title: "Singapore",
+                subtitle: "8 Marina View, Asia Square Tower 1, Level 32 - 06, Singapore 018960",
+                description: "We opened an office in Singapore in 2012."
+            },
+            {
+                title: "Sydney",
+                subtitle: "Level 9, 88 Phillip Street, Sydney, NSW 2000, Australia",
+                description: "We opened our first office in Sydney in 2006."
+            },
+            {
+                title: "Tokyo",
+                subtitle: "20th Fl., Shin-Marunouchi Center Building, 6-2, Marunouchi 1-chome, Chiyoda-ku, Tokyo",
+                description: "We opened an office in Tokyo in 2012."
+            },
+            {
+                title: "Pune, India",
+                subtitle: "",
+                description: "Outsourced service providers. No ICG employees."
+            }
+        ],
+        posts1: [
+            { title: "Our purpose & business", description: "Creating sustainable value by partnering with ambitious businesses" },
+            { title: "Our culture & values", description: "We aim to create a culture in which our employees feel comfortable to bring their entire experience and background to work, in order to develop their talents and deliver exceptional results" },
+            { title: "Leadership & governance", description: "ICG is committed to the highest standards of corporate governance" },
+            { title: "Corporate social responsibility", description: "We look to support organisations that are aligned to our wider corporate objectives and areas of priority" },
+        ],
+        posts2: [
+            { title: "Our history", description: "ICG was founded in 1989 on the principles of flexible investment solutions, specialist experience and local knowledge" },
+            { title: "Our offices", description: "Established in Europe, we have built a global alternative asset management business across sectors and geographies, which are increasingly interconnected" },
+            { title: "Company information", description: "ICG’s registered company name, number and office address" }
+        ],
+        posts3: [
+            { title: "Sustainability", description: "We support responsible and sustainable business practices through our investments and operations" },
+            { title: "People", description: "Our people are fundamental to every part of our success" },
+        ],
+        posts4: [
+            { title: "Clients", description: "Significant and resilient value creation for our clients" },
+            { title: "Shareholders & Debtholders", description: "Over 30 years we’ve built an outstanding track record in creating value and growth through responsible, innovative strategies developed by our diverse global team" },
+        ],
     },
     pt: {
         dynamicTitle: "Um líder global em gestão de ativos alternativos",
@@ -52,6 +160,8 @@ const whoTranslation = {
         },
         global: {
             title: "À primeira vista",
+            globalFootprintTitle: "Pegada global da ICG",
+            globalFootprintDescription: "Operando em 19 locais.",
             stats: [
                 {
                     value: "R$101 bilhões",
@@ -68,8 +178,114 @@ const whoTranslation = {
                     description: "Locais em todo o mundo",
                     subtext: ""
                 }
-            ]
-        }
+            ],
+        },
+        mapPoints: [
+            {
+                title: "Londres",
+                subtitle: "Procession House, 55 Ludgate Hill, London, EC4M 7JW, Inglaterra, Reino Unido",
+                description: "A capital do Reino Unido é a sede global da nossa empresa."
+            },
+            {
+                title: "Amsterdã",
+                subtitle: "Paulus Potterstraat 20, 2hg., Amsterdam, 1071 DA, Países Baixos",
+                description: ""
+            },
+            {
+                title: "Luxemburgo",
+                subtitle: "Centro de Negócios Privados, 32-36 Boulevard d’Avranches, L-1160, Luxemburgo",
+                description: ""
+            },
+            {
+                title: "Madri",
+                subtitle: "Serrano, 30-3°, 28001, Madri, Espanha",
+                description: "Abrimos um escritório em Madri em 2003."
+            },
+            {
+                title: "Milão",
+                subtitle: "Corso Giacomo Matteotti 3, CAP 20121 Milão, Itália",
+                description: "Abrimos um escritório em Milão em 2020."
+            },
+            {
+                title: "Paris",
+                subtitle: "1, rue de la Paix, 75002, Paris, França",
+                description: ""
+            },
+            {
+                title: "Estocolmo",
+                subtitle: "David Bagares Gata, 3, 111 38 Estocolmo, Suécia",
+                description: ""
+            },
+            {
+                title: "Varsóvia",
+                subtitle: "Spark B (7º andar), Aleja Solidarności 171 St., 00-877 Varsóvia, Polônia",
+                description: ""
+            },
+            {
+                title: "Nova Iorque",
+                subtitle: "277 Park Avenue, Nova Iorque, NY 10172, EUA",
+                description: "Abrimos um escritório em Nova Iorque em 2007."
+            },
+            {
+                title: "Toronto",
+                subtitle: "161 Bay Street, Suite 2700, Toronto, Ontário, M5J 2S1",
+                description: "Abrimos um escritório em Toronto em 2024."
+            },
+            {
+                title: "São Francisco",
+                subtitle: "",
+                description: ""
+            },
+            {
+                title: "Dubai",
+                subtitle: "Index Tower, Andar 4, Unidade 404, Centro Financeiro Internacional de Dubai, Dubai, Emirados Árabes Unidos",
+                description: "Abrimos nosso escritório em Dubai em 2021."
+            },
+            {
+                title: "Hong Kong",
+                subtitle: "Suites 1301 – 1302, AIA Central, 1 Connaught Road Central, Hong Kong",
+                description: "Abrimos nosso primeiro escritório na Ásia-Pacífico, em Hong Kong, em 2001."
+            },
+            {
+                title: "Cingapura",
+                subtitle: "8 Marina View, Asia Square Tower 1, Nível 32 - 06, Cingapura 018960",
+                description: "Abrimos um escritório em Cingapura em 2012."
+            },
+            {
+                title: "Sydney",
+                subtitle: "Level 9, 88 Phillip Street, Sydney, NSW 2000, Austrália",
+                description: "Abrimos nosso primeiro escritório em Sydney em 2006."
+            },
+            {
+                title: "Tóquio",
+                subtitle: "20º andar, Edifício Shin-Marunouchi Center, 6-2, Marunouchi 1-chome, Chiyoda-ku, Tóquio",
+                description: "Abrimos um escritório em Tóquio em 2012."
+            },
+            {
+                title: "Pune, Índia",
+                subtitle: "",
+                description: "Fornecedores de serviços terceirizados. Sem funcionários do ICG."
+            }
+        ],
+        posts1: [
+            { title: "Nosso propósito e negócios", description: "Criando valor sustentável ao fazer parceria com empresas ambiciosas" },
+            { title: "Nossa cultura e valores", description: "Nosso objetivo é criar uma cultura na qual nossos funcionários se sintam confortáveis para trazer toda sua experiência e bagagem para o trabalho, a fim de desenvolver seus talentos e entregar resultados excepcionais" },
+            { title: "Liderança e governança", description: "A ICG está comprometida com os mais altos padrões de governança corporativa" },
+            { title: "Responsabilidade social corporativa", description: "Buscamos apoiar organizações que estejam alinhadas com nossos objetivos corporativos mais amplos e áreas de prioridade" },
+        ],
+        posts2: [
+            { title: "Nossa história", description: "A ICG foi fundada em 1989 com os princípios de soluções de investimento flexíveis, experiência especializada e conhecimento local" },
+            { title: "Nossos escritórios", description: "Estabelecida na Europa, construímos um negócio global de gestão de ativos alternativos em setores e geografias, que estão cada vez mais interconectados" },
+            { title: "Informações da empresa", description: "Nome da empresa registrada da ICG, número e endereço do escritório" }
+        ],
+        posts3: [
+            { title: "Sustentabilidade", description: "Apoiamos práticas comerciais responsáveis e sustentáveis por meio de nossos investimentos e operações" },
+            { title: "Pessoas", description: "Nossas pessoas são fundamentais para cada parte do nosso sucesso" },
+        ],
+        posts4: [
+            { title: "Clientes", description: "Criação de valor significativa e resiliente para nossos clientes" },
+            { title: "Acionistas e Credores", description: "Ao longo de 30 anos, construímos um excelente histórico em criar valor e crescimento por meio de estratégias responsáveis e inovadoras desenvolvidas pela nossa diversa equipe global" },
+        ],
     },
     id: {
         dynamicTitle: "Pemimpin global dalam manajemen aset alternatif",
@@ -88,6 +304,8 @@ const whoTranslation = {
         },
         global: {
             title: "Sekilas",
+            globalFootprintTitle: "Jejak global ICG",
+            globalFootprintDescription: "Beroperasi di 19 lokasi.",
             stats: [
                 {
                     value: "$101 miliar",
@@ -105,6 +323,112 @@ const whoTranslation = {
                     subtext: ""
                 }
             ]
-        }
+        },
+        mapPoints: [
+            {
+                title: "London",
+                subtitle: "Procession House, 55 Ludgate Hill, London, EC4M 7JW, Inggris, Inggris Raya",
+                description: "Ibukota Inggris adalah kantor pusat global kami."
+            },
+            {
+                title: "Amsterdam",
+                subtitle: "Paulus Potterstraat 20, 2hg., Amsterdam, 1071 DA, Belanda",
+                description: ""
+            },
+            {
+                title: "Luxemburg",
+                subtitle: "Pusat Bisnis Pribadi, 32-36 Boulevard d’Avranches, L-1160, Luxemburg",
+                description: ""
+            },
+            {
+                title: "Madrid",
+                subtitle: "Serrano, 30-3°, 28001, Madrid, Spanyol",
+                description: "Kami membuka kantor di Madrid pada tahun 2003."
+            },
+            {
+                title: "Milan",
+                subtitle: "Corso Giacomo Matteotti 3, CAP 20121 Milano, Italia",
+                description: "Kami membuka kantor di Milan pada tahun 2020."
+            },
+            {
+                title: "Paris",
+                subtitle: "1, rue de la Paix, 75002, Paris, Prancis",
+                description: ""
+            },
+            {
+                title: "Stockholm",
+                subtitle: "David Bagares Gata, 3, 111 38 Stockholm, Swedia",
+                description: ""
+            },
+            {
+                title: "Warsawa",
+                subtitle: "Spark B (lantai 7), Aleja Solidarności 171 St., 00-877 Warsawa, Polandia",
+                description: ""
+            },
+            {
+                title: "New York",
+                subtitle: "277 Park Avenue, New York, NY 10172, AS",
+                description: "Kami membuka kantor di New York pada tahun 2007."
+            },
+            {
+                title: "Toronto",
+                subtitle: "161 Bay Street, Suite 2700, Toronto, Ontario, M5J 2S1",
+                description: "Kami membuka kantor di Toronto pada tahun 2024."
+            },
+            {
+                title: "San Francisco",
+                subtitle: "",
+                description: ""
+            },
+            {
+                title: "Dubai",
+                subtitle: "Index Tower, Lantai 4, Unit 404, Dubai International Financial Centre, Dubai, Uni Emirat Arab",
+                description: "Kami membuka kantor di Dubai pada tahun 2021."
+            },
+            {
+                title: "Hong Kong",
+                subtitle: "Suites 1301 – 1302, AIA Central, 1 Connaught Road Central, Hong Kong",
+                description: "Kami membuka kantor pertama kami di Asia Pasifik, di Hong Kong, pada tahun 2001."
+            },
+            {
+                title: "Singapura",
+                subtitle: "8 Marina View, Asia Square Tower 1, Lantai 32 - 06, Singapura 018960",
+                description: "Kami membuka kantor di Singapura pada tahun 2012."
+            },
+            {
+                title: "Sydney",
+                subtitle: "Level 9, 88 Phillip Street, Sydney, NSW 2000, Australia",
+                description: "Kami membuka kantor pertama kami di Sydney pada tahun 2006."
+            },
+            {
+                title: "Tokyo",
+                subtitle: "Lantai 20, Gedung Shin-Marunouchi Center, 6-2, Marunouchi 1-chome, Chiyoda-ku, Tokyo",
+                description: "Kami membuka kantor di Tokyo pada tahun 2012."
+            },
+            {
+                title: "Pune, India",
+                subtitle: "",
+                description: "Penyedia layanan pihak ketiga. Tidak ada karyawan ICG."
+            }
+        ],
+        posts1: [
+            { title: "Tujuan & bisnis kami", description: "Menciptakan nilai yang berkelanjutan dengan bermitra dengan bisnis yang ambisius" },
+            { title: "Budaya & nilai kami", description: "Kami bertujuan untuk menciptakan budaya di mana karyawan kami merasa nyaman untuk membawa seluruh pengalaman dan latar belakang mereka ke tempat kerja, untuk mengembangkan bakat mereka dan memberikan hasil yang luar biasa" },
+            { title: "Kepemimpinan & tata kelola", description: "ICG berkomitmen pada standar tata kelola perusahaan yang tertinggi" },
+            { title: "Tanggung jawab sosial perusahaan", description: "Kami berusaha untuk mendukung organisasi yang selaras dengan tujuan korporat kami yang lebih luas dan area prioritas" },
+        ],
+        posts2: [
+            { title: "Sejarah kami", description: "ICG didirikan pada tahun 1989 dengan prinsip solusi investasi yang fleksibel, pengalaman spesialis, dan pengetahuan lokal" },
+            { title: "Kantor kami", description: "Didirikan di Eropa, kami telah membangun bisnis pengelolaan aset alternatif global di berbagai sektor dan geografis, yang semakin saling terhubung" },
+            { title: "Informasi perusahaan", description: "Nama perusahaan terdaftar ICG, nomor, dan alamat kantor" }
+        ],
+        posts3: [
+            { title: "Keberlanjutan", description: "Kami mendukung praktik bisnis yang bertanggung jawab dan berkelanjutan melalui investasi dan operasi kami" },
+            { title: "Orang", description: "Orang-orang kami adalah bagian mendasar dari setiap bagian kesuksesan kami" },
+        ],
+        posts4: [
+            { title: "Klien", description: "Penciptaan nilai yang signifikan dan tangguh bagi klien kami" },
+            { title: "Pemegang Saham & Pemberi Utang", description: "Selama lebih dari 30 tahun, kami telah membangun rekam jejak luar biasa dalam menciptakan nilai dan pertumbuhan melalui strategi yang bertanggung jawab dan inovatif yang dikembangkan oleh tim global kami yang beragam" },
+        ],
     }
 };
